afc9724e7d731715f1030cbb9c40bcedee6d72ba
Justin Gosses <jgosses82@gmail.com>
2021-11-14 21:19:25 -0600

    Update harvest_repos.py

--- python/harvest_repos.py
+++ python/harvest_repos.py
@@ -1,1 +1,13 @@
 print("testing if this python script ran by a print statement")
+
+import markdown
+import re
+
+f = open('myfile.txt', 'r') 
+f.read() 
+
+p = re.compile(https:\/\/github.com\/)\w+(\/)\w+
+
+matched = p.match(f)
+
+print("matched",matched)
